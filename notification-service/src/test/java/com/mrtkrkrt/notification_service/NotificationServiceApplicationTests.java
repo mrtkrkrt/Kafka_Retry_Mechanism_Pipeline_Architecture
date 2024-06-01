6a1976ee2edfeb7f5f7319cada58e74ee04c5373
mrtkrkrt <muratkrkrt0711@gmail.com>
2024-06-02 00:12:33 +0300

    initialize project

---
+++ notification-service/src/test/java/com/mrtkrkrt/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrtkrkrt.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
